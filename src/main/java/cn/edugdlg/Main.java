2a67bbf93e838770485194b5518d0ea69adc7a79
trueikun0504 <131547249+trueikun0504@users.noreply.github.com>
2024-11-15 01:23:53 +0800

    Update Main.java

--- src/main/java/cn/edugdlg/Main.java
+++ src/main/java/cn/edugdlg/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
           System.out.println("Hello world!");
         System.out.println(1);
-        System.out.println(2);//你好重新
+        System.out.println(2);
     }
 }
